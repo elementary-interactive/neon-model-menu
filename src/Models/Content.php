f29ecb7c2c6575e47f02de7ade9db47102614744
Balazs Ercsey <laze@laze.hu>
2023-04-19 13:54:40 +0200

    Upgades - Link is also depends on site. - Content added. - Links can have content, and if link is created, it also creates a content by default. - All things, like Menu, Site, Content can be configured on model level to what class should be creted.

---
+++ src/Models/Content.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Neon\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Neon\Attributable\Models\Traits\Attributables;
+use Neon\Models\Traits\Uuid;
+
+class Content extends Model
+{
+    use Attributable;
+    use Uuid;
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'content' => 'json'
+    ];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'content'
+    ];
+
+    public function __construct()
+    {
+        $this->casts['content'] = class_exists(\Whitecube\NovaFlexibleContent\Value\FlexibleCast::class) ? \Whitecube\NovaFlexibleContent\Value\FlexibleCast::class : 'json';
+    }
+
+    /**
+     *
+     */
+    public function link()
+    {
+        return $this->belongsTo(config('neon.link.model'), 'link_id');
+    }
+}
