ebeab5f29d3ef1233e4e7faf8128a4c76d17333e
Balazs Ercsey <laze@laze.hu>
2023-04-19 13:44:47 +0200

    Add config plus clean up service stuff.

--- src/NeonMenuServiceProvider.php
+++ src/NeonMenuServiceProvider.php
@@ -19,14 +19,20 @@ class NeonMenuServiceProvider extends ServiceProvider
    */
   public function boot(Kernel $kernel): void
   {
+    $this->publishes([
+      __DIR__.'/../config/config.php'   => config_path('neon.php'),
+    ], 'neon-menu');
 
     if ($this->app->runningInConsole()) {
-      if (!class_exists('CreateMenusTable') || !class_exists('CreateLinksTable')) {
-        $this->publishes([
-          __DIR__ . '/../database/migrations/create_menus_pivot.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_menus_table.php'),
-          __DIR__ . '/../database/migrations/create_links_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_links_table.php'),
-        ], 'neon-menu');
+      $migrations = [];
+
+      if (!class_exists('CreateMenusTable')) {
+        $migrations[__DIR__ . '/../database/migrations/create_menus_pivot.php.stub'] = database_path('migrations/' . date('Y_m_d_His', time()) . '_create_menus_table.php');
+      } 
+      if (!class_exists('CreateLinksTable')) {
+        $migrations[__DIR__ . '/../database/migrations/create_links_table.php.stub'] = database_path('migrations/' . date('Y_m_d_His', time()) . '_create_links_table.php');
       }
+      $this->publishes($migrations, 'neon-menu');
     }
 
     $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
