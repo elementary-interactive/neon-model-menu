e58793a0040c684533aaa067077e21516d39c8b1
Balazs Ercsey <laze@laze.hu>
2024-02-04 19:41:36 +0100

    Fix missing stuff, like database migration, etc... Added Spatie Package handler stuff.

--- database/migrations/create_menus_table.php
+++ database/migrations/create_menus_table.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Neon\Models\Statuses\BasicStatus;
 
-class CreateMenusTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
